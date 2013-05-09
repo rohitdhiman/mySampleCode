c1568043dfbec77f6e1dff0fbfc259049622142b
rohit.dhiman <sp.rohit385@gmail.com>
2013-05-09 09:39:31 +0530

    Comment is added.

--- GoogleWeatherDemo/GoogleWeatherDemo/MapViewController.m
+++ GoogleWeatherDemo/GoogleWeatherDemo/MapViewController.m
@@ -33,6 +33,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     _mapView.delegate= self;
+    //for displaying intial position
     [self displayRegion];
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
